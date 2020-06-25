85fb65102234bfaf1d491a6d88b8aec3bce049a5
Alisson Goulart <alissonbrenoemp@gmail.com>
2020-06-25 16:57:22 -0300

    Class 04 - layout design [Part 2]

--- lib/pages/home_page/widgets/poke_item.dart
+++ lib/pages/home_page/widgets/poke_item.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:pokedex/consts/consts_api.dart';
 import 'package:pokedex/consts/consts_app.dart';
 
 class PokeItem extends StatelessWidget {
@@ -21,31 +22,46 @@ class PokeItem extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(4.0),
           child: Stack(
-            //alignment: Alignment.bottomRight,
-            //Parei aqui
             children: <Widget>[
-              Opacity(
-                child: Image.asset(
-                  ConstsApp.whitePokeball,
+              Align(
+                alignment: Alignment.bottomRight,
+                child: Opacity(
+                  child: Image.asset(
+                    ConstsApp.whitePokeball,
+                    height: 100,
+                    width: 100,
+                  ),
+                  opacity: 0.2,
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: CachedNetworkImage(
                   height: 100,
                   width: 100,
+                  placeholder: (context, url) => new Container (
+                    color: Colors.transparent,
+                  ),
+                  imageUrl: 'https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/$num.png',
                 ),
-                opacity: 0.2,
               ),
-              CachedNetworkImage(
-                height: 100,
-                width: 100,
-                placeholder: (context, url) => new Container (
-                  color: Colors.transparent,
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text(
+                  name, 
+                  style: TextStyle(
+                    fontFamily: 'Google',
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white
+                  ),
                 ),
-                imageUrl: 'https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/$num.png',
               ),
-              Text(name),
             ],
           ),
         ),
         decoration: BoxDecoration(
-            color: Colors.green,
+            color: ConstsAPI.getColorType(type: types[0]),
             borderRadius: BorderRadius.all(Radius.circular(20))),
       ),
     );
